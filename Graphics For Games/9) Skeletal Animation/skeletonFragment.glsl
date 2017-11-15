6611a5152fb5b872818ac76f7821ffccff0572e8
Anting Zhou <572480656@qq.com>
2017-11-15 10:11:55 +0800

    2017/11/15

---
+++ Graphics For Games/9) Skeletal Animation/skeletonFragment.glsl
@@ -0,0 +1,21 @@
+#version 150 core
+
+uniform sampler2D diffuseTex;
+
+uniform vec3 cameraPos;
+uniform vec4 lightColour;
+uniform vec3 lightPos;
+uniform float lightRadius;
+
+in Vertex{
+	vec3 colour;
+	vec2 texCoord;
+	vec3 normal;
+	vec3 worldPos;
+} IN;
+
+out vec4 FragColor;
+
+void main(void) {
+	FragColor = texture(diffuseTex, IN.texCoord);
+}
